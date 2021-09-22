0266b153413b3e9e34be1693ff12d4cb134689af
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2021-09-22 13:36:30 +0530

    Time: 7 ms (72.77%), Space: 38.6 MB (53.75%) - LeetHub

---
+++ palindrome-number/palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        if(x<10) return true;
+        String s=Integer.toString(x);
+        int left=0; int right=s.length()-1;
+        while(left<=right){
+            if(s.charAt(left)!=s.charAt(right)) return false;
+            left++; right--;
+        }
+        return true;
+    }
+}
