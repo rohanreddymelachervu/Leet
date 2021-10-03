cb621018d9c299a31d932ee327862b40b042aa60
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2021-10-03 12:14:53 +0530

    Time: 5 ms (30.74%), Space: 38.9 MB (41.96%) - LeetHub

---
+++ truncate-sentence/truncate-sentence.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String result="";
+        String[] words=s.split(" ");
+        for(int i=0;i<k;i++){
+            result+=words[i]+" ";
+        }
+        return result.strip();
+    }
+}
