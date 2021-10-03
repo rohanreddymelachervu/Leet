8b4b5614d10d473838948035e85fc79a43ff34f6
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2021-10-03 13:03:59 +0530

    Time: 2 ms (91.13%), Space: 38.8 MB (95.67%) - LeetHub

--- add-strings/add-strings.java
+++ add-strings/add-strings.java
@@ -1,9 +1,9 @@
 class Solution {
     public String addStrings(String num1, String num2) {
         StringBuilder result=new StringBuilder();
-        int i=num1.length()-1; int j=num2.length()-1; int carry=0;
+        int i=num1.length()-1; int j=num2.length()-1; int carry=0; int sum=0;
         while(i>=0 || j>=0){
-            int sum=carry;
+            sum=carry;
             if(i>=0){
                 sum+=num1.charAt(i--)-'0';
             } if(j>=0){
