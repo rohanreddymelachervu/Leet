019c55cf3c54e7d6dda13182780da1c9401080e3
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2021-10-15 23:34:32 +0530

    Time: 4 ms (63.44%), Space: 42 MB (12.45%) - LeetHub

---
+++ combination-sum/combination-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if(candidates==null) return null;
+        List<List<Integer>> result=new ArrayList<>();
+        finalCombinations(candidates,0,target,new ArrayList<>(),result);
+        return result;
+    }
+    public void finalCombinations(int[] candidates,int index,int target,List<Integer> current,List<List<Integer>> result){
+        if(target==0){
+            result.add(new ArrayList(current));
+            return;
+        }
+        for(int i=index;i<candidates.length;i++){
+            if(candidates[i]<=target){
+                current.add(candidates[i]);
+                finalCombinations(candidates,i,target-candidates[i],current,result);
+                current.remove(new Integer(candidates[i]));
+            }
+        }
+    }
+}
