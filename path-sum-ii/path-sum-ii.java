901f11108291a4edc1c1681df5efd38c849d5e6a
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2021-11-03 21:46:38 +0530

    Time: 2 ms (49.50%), Space: 41.2 MB (48.85%) - LeetHub

--- path-sum-ii/path-sum-ii.java
+++ path-sum-ii/path-sum-ii.java
@@ -15,17 +15,17 @@
  */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> paths=new ArrayList<>();
+        List<List<Integer>> paths=new ArrayList();
         findPaths(root,targetSum,new ArrayList<Integer>(),paths);
         return paths;
     }
     public void findPaths(TreeNode root,int targetSum,List<Integer> current,List<List<Integer>> paths){
         if(root==null) return;
         current.add(root.val);
-            if(root.val==targetSum && root.left==null && root.right==null){
-                paths.add(current);
-                return;
-            }
+        if(root.val==targetSum && root.left==null && root.right==null){
+            paths.add(current);
+            return;
+        }
         findPaths(root.left,targetSum-root.val,new ArrayList<Integer>(current),paths);
         findPaths(root.right,targetSum-root.val,new ArrayList<Integer>(current),paths);
     }
