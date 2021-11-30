b252373f35f0e4cca4e4a4bc793864606df9f7ff
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2021-11-30 17:22:44 +0530

    Time: 94 ms (76.90%), Space: 37 MB (89.19%) - LeetHub

--- word-search/word-search.java
+++ word-search/word-search.java
@@ -2,17 +2,18 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[i].length;j++){
-                if(board[i][j]==word.charAt(0) && DFS(board,i,j,0,word)) return true;
+                if(board[i][j]==word.charAt(0) && DFS(board,word,i,j,0)) return true;
             }
         }
         return false;
     }
-    public boolean DFS(char[][] board,int i,int j,int index,String word){
+    public boolean DFS(char[][]board,String word,int i,int j,int index){
         if(index==word.length()) return true;
-        if(i<0 || j<0 || i>=board.length || j>=board[i].length || word.charAt(index)!=board[i][j]) return false;
+        if(i<0 || j <0 || i>=board.length || j>=board[i].length || word.charAt(index)!=board[i][j]) return false;
         char temp=board[i][j];
         board[i][j]=' ';
-        boolean found=DFS(board,i+1,j,index+1,word) || DFS(board,i-1,j,index+1,word) || DFS(board,i,j+1,index+1,word) || DFS(board,i,j-1,index+1,word);
+        boolean found=DFS(board,word,i+1,j,index+1) || DFS(board,word,i-1,j,index+1) || DFS(board,word,i,j+1,index+1) ||
+            DFS(board,word,i,j-1,index+1);
         board[i][j]=temp;
         return found;
     }
