a54fef4322c51e9501080922d5ff2d59a722b441
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2021-11-30 17:32:40 +0530

    Time: 9 ms (7.83%), Space: 37.3 MB (20.78%) - LeetHub

--- sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -14,23 +14,19 @@
  * }
  */
 class Solution {
-    List<String> numbers=new ArrayList();
-    public void traverse(TreeNode root,String temp){
+    int sum=0;
+    public int sumNumbers(TreeNode root) {
+        if(root==null) return 0;
+        traverse(root,"");
+        return sum;
+    }
+    public void traverse(TreeNode root,String currentSum){
         if(root==null) return;
-        if(root.left!=null) traverse(root.left,temp+Integer.toString(root.val));
-        if(root.right!=null) traverse(root.right,temp+Integer.toString(root.val));
+        if(root.left!=null) traverse(root.left,currentSum+Integer.toString(root.val));
+        if(root.right!=null) traverse(root.right,currentSum+Integer.toString(root.val));
         if(root.right==null && root.left==null){
-            numbers.add(temp+Integer.toString(root.val));
+            sum+=Integer.parseInt(currentSum+root.val);
             return;
         }
-    }
-    public int sumNumbers(TreeNode root) {
-        int sum=0;
-        traverse(root,"");
-        for(String s : numbers){
-            int num=Integer.parseInt(s);
-            sum+=num;
-        }
-        return sum;
     }
 }
