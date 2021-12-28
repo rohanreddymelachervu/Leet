d30f37eab5709ebeee9c04f1ee1a1f060911e397
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2021-12-28 09:20:45 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (96.04%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,35 +1,20 @@
 class Solution {
-    public int[] finder(int[] nums,int index){
-        int[] res=new int[2];
-        int high=index; int low=index;
-        while(true){
-            if(high==nums.length-1) break;
-            if(nums[high]==nums[high+1]) high++;
-            else break;
-        } 
-        while(true){
-            if(low==0) break;
-            if(nums[low]==nums[low-1]) low--;
-            else break;
-        } 
-        res[0]=low; res[1]=high;
-        return res;
-    }
     public int[] searchRange(int[] nums, int target) {
-        int[] temp={0,0};
-        if(nums.length==1){
-            if(nums[0]==target) return temp;
-        }
-        int left=0; int right=nums.length-1;
-        int[] result={-1,-1};
+        int left=0, right=nums.length-1;
         while(left<=right){
             int mid=left+(right-left)/2;
-            if(nums[mid]==target){
-                return finder(nums,mid);
-            }
-            if(nums[mid]<target) left=mid+1;
+            if(nums[mid]==target) return findIndices(nums,mid,target);
+            else if(nums[mid]<target) left=mid+1;
             else right=mid-1;
         }
+        return new int[] {-1,-1};
+    }
+    public int[] findIndices(int[] nums,int index,int target){
+        int[] result=new int[2];
+        int high=index, low=index;
+        while(high<=nums.length-1 && nums[high]==target) ++high;
+        while(low>=0 && nums[low]==target) --low;
+        result[0]=low+1; result[1]=high-1;
         return result;
     }
 }
