f75c1419404d739ca051b97a8032c2220bee4c91
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-01-05 11:12:15 +0530

    Time: 28 ms (9.24%), Space: 150.8 MB (21.43%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> result=new ArrayList();
+        List<List<String>> result=new ArrayList<>();
         List<String> path=new ArrayList<String>();
         func(0,s,path,result);
         return result;
@@ -19,9 +19,9 @@ class Solution {
         }
     }
     public boolean isPalindrome(String s,int start,int end){
-            while(start<=end){
-                if(s.charAt(start++)!=s.charAt(end--)) return false;
-            }
+        while(start<=end){
+            if(s.charAt(start++)!=s.charAt(end--)) return false;
+        }
         return true;
     }
 }
