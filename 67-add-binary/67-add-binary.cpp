13b9390775f5fc802340d960c8c7daefb4c3fb70
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-01-10 10:28:55 +0530

    Time: 4 ms (61.32%), Space: 6.4 MB (59.51%) - LeetHub

---
+++ 67-add-binary/67-add-binary.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result="";
+        int i=a.size()-1, j=b.size()-1, carry=0;
+        while(i>=0 or j>=0){
+            int sum=carry;
+            if(i>=0) sum+=a[i--]-'0';
+            if(j>=0) sum+=b[j--]-'0';
+            result+=to_string(sum%2);
+            carry=sum/2;
+        }
+        if(carry!=0) result+=to_string(carry);
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
