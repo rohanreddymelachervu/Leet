ed460ce3955802ad6a5ab65a9d170c256208ad82
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-01-11 21:37:16 +0530

    Time: 2 ms (99.47%), Space: 39.9 MB (39.97%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -3,19 +3,21 @@ class Solution {
         int max=0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[i].length;j++){
-                if(grid[i][j]==1) max=Math.max(DFS(grid,i,j),max);
+                if(grid[i][j]==1){
+                    max=Math.max(max,DFS(grid,i,j));
+                }
             }
         }
         return max;
     }
     public int DFS(int[][] grid,int i,int j){
         if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j]==0) return 0;
-        grid[i][j]=0;
         int count=1;
+        grid[i][j]=0;
         count+=DFS(grid,i+1,j)
-        +DFS(grid,i-1,j)
-        +DFS(grid,i,j+1)
-        +DFS(grid,i,j-1);
+            +DFS(grid,i-1,j)
+            +DFS(grid,i,j+1)
+            +DFS(grid,i,j-1);
         return count;
     }
 }
