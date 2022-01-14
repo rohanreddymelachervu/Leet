a7de01eb9ee90824e6ff63adea71fd7de2f9b298
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-01-14 12:57:18 +0530

    Time: 1 ms (78.71%), Space: 39.1 MB (75.71%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -6,8 +6,8 @@ class Solution {
     }
     public void backtrack(List<String> result,String current,int open,int close,int max){
         if(current.length()==max*2){
-           result.add(current);
-           return;
+            result.add(current);
+            return;
         }
         if(open<max) backtrack(result,current+"(",open+1,close,max);
         if(close<open) backtrack(result,current+")",open,close+1,max);
