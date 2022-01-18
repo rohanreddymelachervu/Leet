fb2cf52bd584b0b341ce3c5057123237e054503e
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-01-18 10:27:20 +0530

    Time: 0 ms (100.00%), Space: 36.8 MB (61.20%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.java
@@ -15,10 +15,14 @@
  */
 class Solution {
     int sum=0;
-    public void traverse(TreeNode root,boolean isLeft){
-        if(isLeft && root.left==null && root.right==null) sum+=root.val;
-        if(root.left!=null) traverse(root.left,true);
-        if(root.right!=null) traverse(root.right,false);
+    public void traverse(TreeNode root,boolean left){
+        if(root==null) return;
+        traverse(root.left,true);
+        traverse(root.right,false);
+        if(root.left==null && root.right==null && left){
+            sum+=root.val;
+            return;
+        }
     }
     public int sumOfLeftLeaves(TreeNode root) {
         if(root==null) return 0;
