9fa7769303dc652f3dbe029c9eca3180663f3ed1
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-01-18 15:26:18 +0530

    Time: 5 ms (14.21%), Space: 39.3 MB (5.63%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -3,12 +3,11 @@ class Solution {
         if(s.length()%2!=0) return false;
         Stack<Character> stack=new Stack<>();
         for(char ch : s.toCharArray()){
-            if(ch=='{' || ch=='(' || ch=='[')
-                stack.push(ch);
+            if(ch=='(' || ch=='{' || ch=='[') stack.push(ch);
             else{
                 if(stack.isEmpty()) return false;
-                char c=stack.pop();
-                if((ch==')' && c=='(') || (ch=='}' && c=='{') || (ch==']' && c=='[')) continue;
+                char c=stack.peek();
+                if((c=='{' && ch=='}') || (c=='(' && ch==')') || (c=='[' && ch==']')) stack.pop();
                 else return false;
             }
         }
