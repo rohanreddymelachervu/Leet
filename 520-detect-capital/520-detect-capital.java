1af3e95ae385fdfc4d6e36fd5c530d42352232c0
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-01-30 11:04:20 +0530

    Time: 1 ms (99.59%), Space: 40.2 MB (5.07%) - LeetHub

--- 520-detect-capital/520-detect-capital.java
+++ 520-detect-capital/520-detect-capital.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        int count=0;
-        for(char ch : word.toCharArray()){
-            if(Character.isUpperCase(ch)) count++;
-        }
-        if(count==word.length() || count==0) return true;
-        if(count==1 && Character.isUpperCase(word.charAt(0))) return true;
-        else return false;
+        int capitals=0;
+        for(char ch : word.toCharArray())
+            if(Character.isUpperCase(ch)) capitals++;
+        return ((capitals==word.length()) || (capitals==1 && Character.isUpperCase(word.charAt(0))) || capitals==0); 
+
     }
 }
