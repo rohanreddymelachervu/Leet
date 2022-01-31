bf1beabe195e08a7e6d015dc2979efe8fb6ca4be
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-01-31 10:33:08 +0530

    Time: 28 ms (90.82%), Space: 14 MB (96.95%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        result=[]
+        if digits==None or len(digits)==0:
+            return result
+        mapping=['0','1','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+        self.findCombinations(result,digits,mapping,0,'')
+        return result
+    def findCombinations(self,result,digits,mapping,index,current)->None:
+        if index==len(digits):
+            result.append(current)
+            return
+        letters=mapping[int(digits[index])]
+        for i in range(len(letters)):
+            self.findCombinations(result,digits,mapping,index+1,current+letters[i])
