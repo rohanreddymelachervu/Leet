c9ada8830c6570c846af015c3dab00cd6f32a2da
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-02-01 22:27:39 +0530

    Time: 3 ms (37.66%), Space: 43.8 MB (5.37%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,15 +1,15 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result=new ArrayList<>();
-        backtrack(result,"",0,0,n);
+        findCombinations(result,n,"",0,0,0);
         return result;
     }
-    public void backtrack(List<String> result,String current,int open,int close,int max){
-        if(current.length()==max*2){
+    public void findCombinations(List<String> result,int n,String current,int index,int open,int close){
+        if(index==n*2){
             result.add(current);
             return;
         }
-        if(open<max) backtrack(result,current+"(",open+1,close,max);
-        if(close<open) backtrack(result,current+")",open,close+1,max);
+        if(open<n) findCombinations(result,n,current+"(",index+1,open+1,close); 
+        if(close<open) findCombinations(result,n,current+")",index+1,open,close+1);
     }
 }
