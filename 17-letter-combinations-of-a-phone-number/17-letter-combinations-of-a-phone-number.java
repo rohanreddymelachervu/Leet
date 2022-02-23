6a0e1c212124df53b4e1c820022801037c949663
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-02-23 12:39:36 +0530

    Time: 9 ms (22.20%), Space: 43.1 MB (11.94%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> result=new ArrayList<>();
-        if(digits.length()==0) return result;
         HashMap<Integer,String> map=new HashMap<>();
-        map.put(0,"0"); map.put(1,"1"); map.put(2,"abc"); map.put(3,"def");  map.put(4,"ghi");
+        if(digits.length()==0) return result;
+        map.put(0,"0"); map.put(1,"1"); map.put(2,"abc"); map.put(3,"def"); map.put(4,"ghi");
         map.put(5,"jkl"); map.put(6,"mno"); map.put(7,"pqrs"); map.put(8,"tuv"); map.put(9,"wxyz");
         findCombinations(result,map,digits,"",0);
         return result;
