64a462e322980d2ea211ea84d584223da6dc99d8
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-02-23 12:48:16 +0530

    Time: 48 ms (38.25%), Space: 13.9 MB (92.48%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,15 +1,18 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         result=[]
-        if digits==None or len(digits)==0:
+        if len(digits)==0:
             return result
-        mapping=['0','1','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
-        self.findCombinations(result,digits,mapping,0,'')
+        maps={}
+        maps[0]='0'; maps[1]='1'; maps[2]='abc'; maps[3]='def'; maps[4]='ghi'; maps[5]='jkl'; maps[6]='mno';
+        maps[7]='pqrs'; maps[8]='tuv'; maps[9]='wxyz';
+        self.find_combinations(result,maps,digits,'',0)
         return result
-    def findCombinations(self,result:List[str],digits:str,mapping:List[str],index:int,current:str)->None:
+    def find_combinations(self,result,maps,digits,current,index):
         if index==len(digits):
             result.append(current)
             return
-        letters=mapping[int(digits[index])]
+        letters=maps[(int)(digits[index])]
         for i in range(len(letters)):
-            self.findCombinations(result,digits,mapping,index+1,current+letters[i])
+            self.find_combinations(result,maps,digits,current+letters[i],index+1)
+        
