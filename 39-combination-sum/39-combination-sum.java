5c9af2446682a49da776a70ed09250ae23ef8b24
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-06-14 12:04:00 +0530

    Time: 4 ms (81.51%), Space: 45.7 MB (22.20%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,22 +1,20 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result=new ArrayList<>();
-        findCombinations(result,candidates,target,new ArrayList<>(),0);
+        if(candidates==null || candidates.length==0) return result;
+        backtrack(result,candidates,target,new ArrayList<>(),0);
         return result;
     }
-    public void findCombinations(List<List<Integer>> result,int[] candidates,int target,List<Integer> current,
-                                int index){
+    private void backtrack(List<List<Integer>> result,int[] candidates,int target,List<Integer> current,int index){
         if(index==candidates.length){
-            if(target==0){
-                result.add(new ArrayList<>(current));
-            }
+            if(target==0) result.add(new ArrayList<>(current));
             return;
         }
         if(candidates[index]<=target){
             current.add(candidates[index]);
-            findCombinations(result,candidates,target-candidates[index],current,index);
+            backtrack(result,candidates,target-candidates[index],current,index);
             current.remove(current.size()-1);
         }
-        findCombinations(result,candidates,target,current,index+1);
+        backtrack(result,candidates,target,current,index+1);
     }
 }
