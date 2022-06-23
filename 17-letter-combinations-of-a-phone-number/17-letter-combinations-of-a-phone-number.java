a1442e6e5df9dbc092a1442b07d46647d7fa574f
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-06-23 23:28:05 +0530

    Time: 1 ms (94.03%), Space: 42.7 MB (53.53%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,22 +1,21 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> result=new ArrayList<>();
-        HashMap<Integer,String> map=new HashMap<>();
         if(digits.length()==0) return result;
-        map.put(0,"0"); map.put(1,"1"); map.put(2,"abc"); map.put(3,"def"); map.put(4,"ghi");
-        map.put(5,"jkl"); map.put(6,"mno"); map.put(7,"pqrs"); map.put(8,"tuv"); map.put(9,"wxyz");
-        findCombinations(result,map,digits,"",0);
+        String[] mapping={"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        backtrack(result,digits,mapping,new StringBuilder(),0);
         return result;
     }
-    public void findCombinations(List<String> result,HashMap<Integer,String> map,String digits,String current,
-                                int index){
-        if(index==digits.length()){
-            result.add(current);
+    private void backtrack(List<String> result,String digits,String[] mapping,StringBuilder sb,int index){
+        if(sb.length()==digits.length()){
+            result.add(new String(sb));
             return;
         }
-        String letters=map.get(digits.charAt(index)-'0');
+        String letters=mapping[digits.charAt(index)-'0'];
         for(int i=0;i<letters.length();i++){
-            findCombinations(result,map,digits,current+letters.charAt(i),index+1);
+            sb.append(letters.charAt(i));
+            backtrack(result,digits,mapping,sb,index+1);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
 }
