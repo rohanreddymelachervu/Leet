04902394c3e36d2e70db420985d3f5c234c68c1f
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-06-23 23:12:47 +0530

    Time: 20 ms (41.35%), Space: 195.4 MB (29.35%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,23 +1,23 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> result=new ArrayList<>();
-        findCombs(0,s,new ArrayList<>(),result);
+        backtrack(result,s,new ArrayList<>(),0);
         return result;
     }
-    public void findCombs(int index,String s,List<String> current,List<List<String>> result){
+    private void backtrack(List<List<String>> result,String s,List<String> current,int index){
         if(index==s.length()){
-            result.add(new ArrayList<String>(current));
+            result.add(new ArrayList<>(current));
             return;
         }
         for(int i=index;i<s.length();i++){
             if(isPalindrome(s,index,i)){
                 current.add(s.substring(index,i+1));
-                findCombs(i+1,s,current,result);
+                backtrack(result,s,current,i+1);
                 current.remove(current.size()-1);
             }
         }
     }
-    public boolean isPalindrome(String s,int start,int end){
+    private boolean isPalindrome(String s,int start,int end){
         while(start<=end){
             if(s.charAt(start++)!=s.charAt(end--)) return false;
         }
