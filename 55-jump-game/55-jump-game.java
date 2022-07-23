b9f72fa7fae3138f5b8618b1dcb0f3517e481ce2
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-07-24 01:18:07 +0530

    Time: 2 ms (92.48%), Space: 68.1 MB (44.52%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int lastGoodIndexPosition = nums.length-1;
-        for(int i=nums.length-1;i>=0;i--){
-            if(i+nums[i]>=lastGoodIndexPosition){
-                lastGoodIndexPosition=i;
+        int lastPos=nums.length-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(i+nums[i]>=lastPos){
+                lastPos=i;
             }
         }
-        return lastGoodIndexPosition==0;
+        return lastPos==0;
     }
 }
