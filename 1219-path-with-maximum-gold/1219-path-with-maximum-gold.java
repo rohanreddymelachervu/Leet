6f1f4c73089951da285c57bfe09e9fc783bff8cf
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-07-26 12:30:43 +0530

    Time: 112 ms (60.21%), Space: 42.1 MB (34.26%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -2,12 +2,10 @@ class Solution {
     public int getMaximumGold(int[][] grid) {
         if(grid==null || grid.length==0) return 0;
         int max=0, m=grid.length, n=grid[0].length;
-        boolean[][] visited=new boolean[grid.length][grid[0].length];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]!=0){
-                    int gold=dfs(grid,i,j,m,n,visited);
-                    max=Math.max(max,gold);
+                    max=Math.max(max,dfs(grid,i,j,m,n,new boolean[m][n]));
                 }
             }
         }
@@ -16,11 +14,11 @@ class Solution {
     private int dfs(int[][] grid,int i,int j,int m,int n,boolean[][] visited){
         if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==0 || visited[i][j]) return 0;
         visited[i][j]=true;
-        int left=dfs(grid,i,j-1,m,n,visited);
-        int right=dfs(grid,i,j+1,m,n,visited);
         int up=dfs(grid,i-1,j,m,n,visited);
         int down=dfs(grid,i+1,j,m,n,visited);
+        int left=dfs(grid,i,j-1,m,n,visited);
+        int right=dfs(grid,i,j+1,m,n,visited);
         visited[i][j]=false;
-        return Math.max(left,Math.max(right,Math.max(up,down))) + grid[i][j];
+        return Math.max(left,Math.max(up,Math.max(right,down))) + grid[i][j];
     }
 }
