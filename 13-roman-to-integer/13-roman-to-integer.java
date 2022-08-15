d03b05f48e1e12daa557b2f3411d6757954f5483
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-15 10:57:49 +0530

    Time: 6 ms (85.69%), Space: 45.8 MB (54.82%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,16 +1,17 @@
 class Solution {
     public int romanToInt(String s) {
         int[] map=new int[26];
+        map['I'-'A']=1; map['V'-'A']=5; map['X'-'A']=10;
+        map['L'-'A']=50; map['C'-'A']=100; map['D'-'A']=500; map['M'-'A']=1000;
         int result=0;
-        map['I'-'A']=1; map['V'-'A']=5; map['X'-'A']=10; map['L'-'A']=50;
-        map['C'-'A']=100; map['D'-'A']=500; map['M'-'A']=1000;
         for(int i=0;i<s.length();i++){
-            if(i>0 && map[s.charAt(i)-'A']>map[s.charAt(i-1)-'A']){
-                result+=map[s.charAt(i)-'A']-2*map[s.charAt(i-1)-'A'];
+            if(i+1<s.length() && map[s.charAt(i)-'A']<map[s.charAt(i+1)-'A']){
+                result-=map[s.charAt(i)-'A'];
             }else{
                 result+=map[s.charAt(i)-'A'];
             }
         }
         return result;
+        
     }
 }
