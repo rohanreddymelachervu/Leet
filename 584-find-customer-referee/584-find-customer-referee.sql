387d7acf96e80e903e95f4b1074b203b03b9cfe0
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-17 00:50:18 +0530

    Time: 853 ms (25.48%), Space: 0B (100.00%) - LeetHub

--- 584-find-customer-referee/584-find-customer-referee.sql
+++ 584-find-customer-referee/584-find-customer-referee.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select Customer.name from Customer where Customer.id not in
+select Customer.name as name from Customer where Customer.id not in
 (
     select Customer.id from Customer where Customer.referee_id=2
 );
