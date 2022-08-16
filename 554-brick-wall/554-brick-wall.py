be8efd113a6260c0864d3bb1344a0624c6147c3e
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-16 23:15:17 +0530

    Time: 331 ms (32.12%), Space: 19 MB (62.27%) - LeetHub

---
+++ 554-brick-wall/554-brick-wall.py
@@ -0,0 +1,10 @@
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        countGap = {0 : 0} # map pos to count of brick gaps
+        for r in wall:
+            total = 0
+            for b in r[:-1]:
+                total+=b
+                countGap[total] = 1 + countGap.get(total,0)
+        return len(wall) - max(countGap.values())
+        
