9d87d6a45f53e48e03c4e5df10730243a9cff0ed
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-23 01:50:29 +0530

    Time: 2184 ms (12.21%), Space: 19.6 MB (96.18%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
@@ -4,10 +4,10 @@ class Solution:
         seen = set()
         left = -1
         right = 0
-        while right < len(word):
+        while right<len(word):
             if right>0 and word[right]<word[right-1]:
                 seen.clear()
-                left = right - 1
+                left = right-1
             seen.add(word[right])
             if len(seen) == 5:
                 res = max(res, right-left)
