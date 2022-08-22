678ecc823eac584d8630da8b042fdaca03665d08
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-23 01:46:30 +0530

    Time: 1384 ms (36.64%), Space: 19.6 MB (86.64%) - LeetHub

---
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestBeautifulSubstring(self, word: str) -> int:
+        res = 0
+        seen = set()
+        left = -1
+        right = 0
+        while right < len(word):
+            if right>0 and word[right]<word[right-1]:
+                seen.clear()
+                left = right - 1
+            seen.add(word[right])
+            if len(seen) == 5:
+                res = max(res, right-left)
+            right += 1
+        return res
