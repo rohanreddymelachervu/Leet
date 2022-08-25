c6b1bb94672cdf96e3a0e5483460eb9e95084b07
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-25 11:19:26 +0530

    Time: 23 ms (36.50%), Space: 135.9 MB (76.80%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,25 +1,27 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> result=new ArrayList<>();
-        backtrack(result,s,new ArrayList<>(),0);
+        List<List<String>> result = new ArrayList<>();
+        List<String> curr = new ArrayList<>();
+        backtrack(result, curr, 0, s);
         return result;
     }
-    private void backtrack(List<List<String>> result,String s,List<String> current,int index){
-        if(index==s.length()){
-            result.add(new ArrayList<>(current));
+    public void backtrack(List<List<String>> result, List<String> curr, int index, String s){
+        if(index == s.length()){
+            result.add(new ArrayList<>(curr));
             return;
         }
-        for(int i=index;i<s.length();i++){
-            if(isPalindrome(s,index,i)){
-                current.add(s.substring(index,i+1));
-                backtrack(result,s,current,i+1);
-                current.remove(current.size()-1);
+        for(int i=index; i<s.length(); i++){
+            if(isPalindrome(index, i, s)){
+                String substring = s.substring(index, i+1);
+                curr.add(substring);
+                backtrack(result, curr, i+1, s);
+                curr.remove(curr.size()-1);
             }
         }
     }
-    private boolean isPalindrome(String s,int start,int end){
+    public boolean isPalindrome(int start, int end, String s){
         while(start<=end){
-            if(s.charAt(start++)!=s.charAt(end--)) return false;
+            if(s.charAt(start++) != s.charAt(end--)) return false;
         }
         return true;
     }
