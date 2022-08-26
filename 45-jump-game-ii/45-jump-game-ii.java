747b88ee99913c043773501d5282b4a4dfd41069
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-26 10:01:13 +0530

    Time: 1 ms (99.88%), Space: 42.9 MB (91.56%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,21 +1,16 @@
 class Solution {
-    Integer[] cache;
     public int jump(int[] nums) {
-        cache=new Integer[nums.length+1];
-        return helper(nums, 0);
-    }
-    public int helper(int[] nums, int index){
-        if(index>=nums.length-1){
-            return 0;
+        int result=0;
+        int l=0, r=0;
+        while(r<nums.length-1){
+            int farthest=0;
+            for(int i=l; i<r+1; i++){
+                farthest=Math.max(farthest, nums[i]+i);
+            }
+            l=r+1;
+            r=farthest;
+            result++;
         }
-        if(cache[index]!=null){
-            return cache[index];
-        }
-        int minJumps=10000;
-        for(int i=index+1; i<=index+nums[index]; i++){
-            minJumps=Math.min(minJumps, 1+helper(nums, i));
-        }
-        cache[index]=minJumps;
-        return minJumps;
+        return result;
     }
 }
