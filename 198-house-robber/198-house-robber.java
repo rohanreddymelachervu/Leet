bd7cf4188f2d8d6f039e61f16a314144e8f7abcb
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-26 16:21:10 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (84.04%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,16 +1,14 @@
 class Solution {
-    Integer[] cache;
-    public int helper(int ind, int[] nums, Integer[] cache){
-        if(ind==0) return nums[0];
-        if(ind<0) return 0;
-        if(cache[ind]!=null) return cache[ind];
-        int take=nums[ind] + helper(ind-2, nums, cache);
-        int nonTake = helper(ind-1, nums, cache);
-        return cache[ind] = Math.max(take, nonTake);
-    }
     public int rob(int[] nums) {
-        int n=nums.length;
-        cache=new Integer[n+1];
-        return helper(n-1, nums, cache);
+        int prev=nums[0], prev2=0;
+        for(int i=0; i<nums.length; i++){
+            int take=nums[i];
+            if(i>1) take += prev2;
+            int nonTake=prev;
+            int curr=Math.max(take, nonTake);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
     }
 }
