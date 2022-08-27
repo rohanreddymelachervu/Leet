971adde8060f6999d7f59cfe8064f22f1d0b9680
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-27 23:57:34 +0530

    Time: 1703 ms (5.01%), Space: 629.4 MB (5.01%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.java
@@ -3,7 +3,7 @@ class Solution {
         for(int i=1; i<=s.length()/2; i++){
             String substring=s.substring(0,i);
             StringBuilder sb=new StringBuilder();
-            int numRepeats=s.length()/substring.length();
+            int numRepeats=s.length()/i;
             for(int j=0; j<numRepeats; j++) sb.append(substring);
             if(sb.toString().equals(s)) return true;
         }
