404c058ec66344fd84628cc54cc49f5ee7762062
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-29 16:32:32 +0530

    Time: 93 ms (12.39%), Space: 72.4 MB (7.77%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -18,18 +18,4 @@ class Solution {
         }
         return dp[n-1][amount] == 1e9 ? -1 : dp[n-1][amount];
     }
-    public int helper(int ind, int[] coins, int T, Integer[][] dp){
-        if(ind==0){
-            if(T%coins[ind]==0){
-                return T/coins[ind];
-            }else return (int)1e9;
-        }
-        if(dp[ind][T]!=null) return dp[ind][T];
-        int nonTake=0+helper(ind-1, coins, T, dp);
-        int take=(int)1e9;
-        if(coins[ind]<=T){
-            take=1+helper(ind, coins, T-coins[ind], dp);
-        }
-        return dp[ind][T] = Math.min(nonTake, take);
-    }
 }
