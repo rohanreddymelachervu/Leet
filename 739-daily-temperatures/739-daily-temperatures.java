30b81682423b6af0bfa0d2fc0e123c1dfa168d93
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-29 12:09:26 +0530

    Time: 222 ms (17.56%), Space: 131.2 MB (43.98%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,14 +1,19 @@
 class Solution {
     public int[] dailyTemperatures(int[] t) {
         int[] result=new int[t.length];
-        Arrays.fill(result,0);
+        Arrays.fill(result, 0);
         Stack<Pair<Integer,Integer>> stack=new Stack<>();
-        for(int i=0;i<t.length;i++){
-            while(!stack.isEmpty() && t[i]>stack.peek().getKey()){
-                Pair<Integer,Integer> pop=stack.pop();
-                result[pop.getValue()] = i-pop.getValue();
+        for(int i=0; i<t.length; i++){
+            int currTemp=t[i], currIndex=i;
+            if(stack.isEmpty() || stack.peek().getKey()>currTemp){
+                stack.push(new Pair<>(currTemp, currIndex));
+            }else{
+                while(!stack.isEmpty() && stack.peek().getKey()<currTemp){
+                    Pair<Integer,Integer> top=stack.pop();
+                    result[top.getValue()]=currIndex-top.getValue();
+                }
+                stack.push(new Pair<>(currTemp, currIndex));
             }
-            stack.push(new Pair<>(t[i],i));
         }
         return result;
     }
