a32d5e4e8872c1216f83becb64456faaacb45ab1
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-29 16:09:20 +0530

    Time: 65 ms (18.71%), Space: 71.3 MB (8.22%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,20 +1,22 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        Integer[][] dp=new Integer[coins.length+1][amount+1];
-        int ans=helper(coins.length, coins, amount, dp);
+        int n=coins.length;
+        Integer[][] dp=new Integer[n][amount+1];
+        int ans=helper(n-1, coins, amount, dp);
         return ans == (int)1e9 ? -1 : ans;
     }
-    public int helper(int ind, int[] coins, int amount, Integer[][] dp){
-        if(ind==0 || amount==0){
-            return amount==0 ? 0 : (int)1e9;
+    public int helper(int ind, int[] coins, int T, Integer[][] dp){
+        if(ind==0){
+            if(T%coins[ind]==0){
+                return T/coins[ind];
+            }else return (int)1e9;
         }
-        int min1=(int)1e9;
-        int min2=(int)1e9;
-        if(dp[ind][amount]!=null) return dp[ind][amount];
-        if(amount-coins[ind-1]>=0){
-            min1=helper(ind, coins, amount-coins[ind-1], dp)+1;
+        if(dp[ind][T]!=null) return dp[ind][T];
+        int nonTake=0+helper(ind-1, coins, T, dp);
+        int take=(int)1e9;
+        if(coins[ind]<=T){
+            take=1+helper(ind, coins, T-coins[ind], dp);
         }
-        min2=helper(ind-1, coins, amount, dp);
-        return dp[ind][amount] = Math.min(min1, min2);
+        return dp[ind][T] = Math.min(nonTake, take);
     }
 }
