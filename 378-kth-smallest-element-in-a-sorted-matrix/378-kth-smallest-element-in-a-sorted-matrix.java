fe92fd538186aee9488e0c7e8ccc9e9e9b9a518e
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-29 21:27:33 +0530

    Time: 1 ms (89.37%), Space: 57.1 MB (47.58%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,27 +1,31 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int n=matrix.length, lo=matrix[0][0], hi=matrix[n-1][n-1];
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            int count=countNonBigger(mid, matrix);
-            if(count<k){
-                lo=mid+1;
-            }else{
-                hi=mid-1;
+        int rows=matrix.length, cols=matrix[0].length;
+        int low=matrix[0][0], high=matrix[rows-1][cols-1];
+        int ans=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int num=countNums(matrix, mid);
+            if(num<k){
+                low=mid+1;
+            }else if(num>=k){
+                ans=mid;
+                high=mid-1;
             }
         }
-        return lo;
+        return ans;
     }
-    private int countNonBigger(int target, int[][] matrix){
-        int n=matrix.length, i=n-1, j=0, cnt=0;
-        while(i>=0 && j<n){
-            if(matrix[i][j]>target) i--;
-            else{
-                cnt+=i+1;
+    public int countNums(int[][] matrix, int mid){
+        int i=matrix.length-1, j=0, count=0;
+        while(i>=0 && j<matrix[0].length){
+            int x=matrix[i][j];
+            if(x>mid){
+                i--;
+            }else{
+                count += i+1;
                 j++;
             }
         }
-        return cnt;
+        return count;
     }
-    
 }
