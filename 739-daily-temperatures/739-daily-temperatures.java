76cbe11b5a9e716f8f1909b090e7d8aaa007fa1f
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-29 12:12:53 +0530

    Time: 186 ms (34.27%), Space: 124.7 MB (76.41%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -6,14 +6,14 @@ class Solution {
         for(int i=0; i<t.length; i++){
             int currTemp=t[i], currIndex=i;
             if(stack.isEmpty() || stack.peek().getKey()>currTemp){
-                stack.push(new Pair<>(currTemp, currIndex));
+    
             }else{
                 while(!stack.isEmpty() && stack.peek().getKey()<currTemp){
                     Pair<Integer,Integer> top=stack.pop();
                     result[top.getValue()]=currIndex-top.getValue();
                 }
-                stack.push(new Pair<>(currTemp, currIndex));
             }
+            stack.push(new Pair<>(currTemp, currIndex));
         }
         return result;
     }
