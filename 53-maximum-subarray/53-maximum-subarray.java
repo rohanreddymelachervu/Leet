d7153b20d5a304f8dd713c98558aa608dafdf95d
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-31 22:41:54 +0530

    Time: 2 ms (74.38%), Space: 81.8 MB (13.30%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,9 +1,9 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int localSum=nums[0], globalSum=nums[0];
-        for(int i=1;i<nums.length;i++){
-            localSum=Math.max(localSum+nums[i],nums[i]);
-            globalSum=Math.max(globalSum,localSum);
+        for(int i=1; i<nums.length; i++){
+            localSum=Math.max(nums[i], nums[i]+localSum);
+            globalSum=Math.max(localSum, globalSum);
         }
         return globalSum;
     }
