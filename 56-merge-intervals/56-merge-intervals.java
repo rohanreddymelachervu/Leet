91ef488046c044a697cce681d2d958dfbd16c620
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-31 22:49:06 +0530

    Time: 16 ms (37.68%), Space: 55.8 MB (12.98%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<Pair<Integer,Integer>> result=new ArrayList<>();
-        if(intervals.length==1){
-            return intervals;
-        }
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        List<int[]> result=new ArrayList<>();
         int prevStart=intervals[0][0], prevEnd=intervals[0][1];
-        for(int i=1;i<intervals.length;i++){
-            if(prevEnd>=intervals[i][0]){
-                prevStart=Math.min(prevStart,intervals[i][0]);
-                prevEnd=Math.max(prevEnd,intervals[i][1]);
+        for(int i=1; i<intervals.length; i++){
+            int currStart=intervals[i][0], currEnd=intervals[i][1];
+            if(prevEnd>=currStart){
+                prevStart = Math.min(currStart, prevStart);
+                prevEnd = Math.max(prevEnd, currEnd);
             }else{
-                result.add(new Pair<>(prevStart,prevEnd));
-                prevStart=intervals[i][0]; prevEnd=intervals[i][1];
+                result.add(new int[] {prevStart, prevEnd});
+                prevStart=currStart; 
+                prevEnd=currEnd;
             }
         }
-        result.add(new Pair<>(prevStart,prevEnd));
-        int[][] arr=new int[result.size()][2];
-        for(int i=0;i<result.size();i++){
-            Pair<Integer,Integer> curr=result.get(i);
-            arr[i][0]=curr.getKey();
-            arr[i][1]=curr.getValue();
+        result.add(new int[] {prevStart, prevEnd});
+        int[][] res = new int[result.size()][2];
+        for(int i=0; i<result.size(); i++){
+            res[i]=result.get(i);
         }
-        return arr;
+        return res;
     }
 }
