503801d1594fd82b386d1bf65022e746f7ad9f19
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-08-31 22:23:25 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (72.50%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,5 +1,23 @@
 class Solution {
+    public void swap(int[] nums, int l, int r){
+        int temp=nums[l];
+        nums[l]=nums[r];
+        nums[r]=temp;
+    }
+    public void quicksort(int[] nums, int lo, int hi){
+        if(lo>hi) return;
+        int pivot=nums[hi];
+        int l=lo, r=hi;
+        while(l < r){
+            while(nums[l]<=pivot && l<r) l++;
+            while(nums[r]>=pivot && l<r) r--;
+            swap(nums, l, r);
+        }
+        swap(nums, l, hi);
+        quicksort(nums, lo, l-1);
+        quicksort(nums, l+1, hi);
+    }
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        quicksort(nums, 0, nums.length-1);
     }
 }
