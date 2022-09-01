f048eac5bed407f082d147895c036f3b72804072
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-09-01 16:33:19 +0530

    Time: 5 ms (23.67%), Space: 45.1 MB (23.27%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.java
+++ 841-keys-and-rooms/841-keys-and-rooms.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        HashSet<Integer> visited = new HashSet<>();
-        visited.add(0);
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack=new Stack<>();
+        HashSet<Integer> visited=new HashSet<>();
         stack.push(0);
+        visited.add(0);
         while(!stack.isEmpty()){
             List<Integer> keys = rooms.get(stack.pop());
             for(int key : keys){
