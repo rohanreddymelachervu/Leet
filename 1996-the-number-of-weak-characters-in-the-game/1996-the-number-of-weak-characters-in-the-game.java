5c74e57530cc0f2f86c0f072f2c4802dfbf9b5e5
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-09-10 13:28:13 +0530

    Time: 209 ms (27.38%), Space: 130.8 MB (50.64%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -1,13 +1,12 @@
 class Solution {
     public int numberOfWeakCharacters(int[][] arr) {
-        int n=arr.length;
-        int count=0;
-        Arrays.sort(arr,(p,q)->(p[0]==q[0]?p[1]-q[1]:q[0]-p[0]));
-        int max=Integer.MIN_VALUE;
-        for(int i=0; i<n; i++){
+        var n=arr.length;
+        Arrays.sort(arr, (p,q) -> p[0]==q[0] ? p[1]-q[1] : q[0]-p[0]);
+        int max=Integer.MIN_VALUE, result=0;
+        for(var i=0; i<n; i++){
             max=Math.max(max, arr[i][1]);
-            if(arr[i][1]<max) count++;
+            if(arr[i][1]<max) result++;
         }
-        return count;
+        return result;
     }
 }
