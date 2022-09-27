1130988f8f3242b6c497f051d93257054b115781
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-09-27 22:57:22 +0530

    Time: 22 ms (98.52%), Space: 46.7 MB (90.43%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,18 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         for(int i=0; i<nums.length; i++){
-            if(i==0 || (i>0 && nums[i-1]!=nums[i])){
-                int lo=i+1, hi=nums.length-1, sum=0-nums[i];
-                while(lo < hi){
-                    if(nums[lo] + nums[hi] == sum){
-                        result.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
-                        while(lo<hi && nums[lo]==nums[lo+1]) lo++;
-                        while(lo<hi && nums[hi]==nums[hi-1]) hi--;
-                        lo++; hi--;
-                    }else if(nums[lo] + nums[hi] > sum) hi--;
-                    else lo++;
+            if(i>0 && nums[i-1] == nums[i]){
+                continue;
+            }
+            int lo = i+1, hi = nums.length-1;
+            while(lo < hi){
+                if(nums[i] + nums[lo] + nums[hi] == 0){
+                    result.add(Arrays.asList(nums[lo], nums[i], nums[hi]));
+                    while(lo < hi && nums[lo] == nums[lo+1]) lo++;
+                    while(lo < hi && nums[hi] == nums[hi-1]) hi--;
+                    hi--;
+                    lo++;
+                }else if(nums[i] + nums[lo] + nums[hi] > 0){
+                    hi--;
+                }else{
+                    lo++;
                 }
             }
         }
