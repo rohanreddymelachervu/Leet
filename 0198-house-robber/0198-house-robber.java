e238965dcd4da40a30a79b452f2018e9cbd5d9de
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2022-10-24 17:08:34 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (43.94%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,21 @@
+class Solution {
+    private int f(int ind, int[] nums, Integer[] dp){
+        if(ind == 0){
+            return nums[ind];
+        }
+        if(ind < 0){
+            return 0;
+        }
+        if(dp[ind]!=null){
+            return dp[ind];
+        }
+        int take = nums[ind] + f(ind-2, nums, dp);
+        int notTake = f(ind-1, nums, dp);
+        return dp[ind] = Math.max(take, notTake);
+    }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        Integer[] dp = new Integer[nums.length+1];
+        return f(n-1, nums, dp);
+    }
+}
